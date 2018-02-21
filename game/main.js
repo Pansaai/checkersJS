3c708e9ef0d247100c62af3ec05797b0d0e6af3c
Pansaai <pans.thomas@gmail.com>
2018-02-21 16:44:01 +0900

    Added King Check Method

--- game/main.js
+++ game/main.js
@@ -3,13 +3,12 @@ import Cell from './cell.js';
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
-
 //Global variables
 const rows = 8;
 const cols = 8;
-const boardHeight = 400;
-const boardWidth = 400;
-const size = 50;
+const boardHeight = 700;
+const boardWidth = 700;
+const size = 88;
 
 // Game Variables
 let grid;
@@ -20,8 +19,6 @@ let selectedPiece;
 let jumpedPiece;
 let selectedCol;
 let selectedRow;
-
-
 
 // pieces
 let empty = 0;
@@ -45,7 +42,14 @@ let pieces = [
 render();
 
 
- export function make2DArray(cols,rows){
+/*************************************************************/
+/***                                                       ***/
+/***                     SETUP FUNCTIONS                   ***/
+/***                                                       ***/
+/*************************************************************/
+
+
+  function make2DArray(cols,rows){
   let arr = new Array(cols);
   for(let i = 0; i < arr.length; i++){
     arr[i] = new Array(rows);
@@ -54,7 +58,6 @@ render();
 }
 
 
-// make a new cell for each
 function setupBoard(){
   grid = make2DArray( cols, rows );
 
@@ -65,6 +68,7 @@ function setupBoard(){
     }
   }
 }
+
 
 function drawBoard(){
   setupBoard();
@@ -115,7 +119,7 @@ function DrawPieces(){
       if(pieces[j][i] === king) {
         context.beginPath();
         context.arc(i*size + size/2, j*size + size/2, size/3 , 0, 2*Math.PI);
-        context.fillStyle = '##FFCC00';
+        context.fillStyle = '#696969';
         context.fill(); 
         context.closePath();
       }
@@ -130,6 +134,13 @@ function getMousePos(mouse) {
 
   mousePos = [Math.floor(mouseX/size),Math.floor(mouseY/size)];
 }
+
+
+/*************************************************************/
+/***                                                       ***/
+/***                    GAME FUNCTIONS                     ***/
+/***                                                       ***/
+/*************************************************************/
 
 
 canvas.addEventListener('mousedown', selectPiece, false);
@@ -153,8 +164,6 @@ function selectPiece(){
         // set selectedPiece
         selectedPiece = pieces[j][i];
 
-        console.log(selectedPiece);
-
         // set the value to 0
         pieces[j][i] = 0;
       }
@@ -177,17 +186,23 @@ function pieceDrag(){
   // draw piece on mousePosition
   context.beginPath();
   context.arc(mousePos[0]*size + size/2, mousePos[1]*size + size/2, size/3 , 0, 2*Math.PI);
-  // check for the piececolor
-  if(selectedPiece == 1)
-  {
-    context.fillStyle = '#000';
+
+  if(selectedPiece){
+    // check for the piececolor
+    if(selectedPiece == 1)
+    {
+      context.fillStyle = '#000';
+    }
+    else if( selectedPiece == 2)
+    {
+      context.fillStyle = '#fff';
+    }
+    else if (selectedPiece == 3){
+      context.fillStyle = '#696969';
+    }
+    context.fill();
+    context.closePath();
   }
-   else if( selectedPiece == 2)
-  {
-    context.fillStyle = '#fff';
-  }
-  context.fill();
-  context.closePath();
 }
 
 
@@ -195,19 +210,19 @@ function pieceDrop(){
 
   getMousePos(event);
 
-  // check if the position to drop already has a piece
-  let isPiece = hasPiece();
+  const isPiece = hasPiece();
+  const isDiagonal = isDiagonalMove();
+  const isJump = isJumpMove();
+  const becomesKing = isKing();
 
-  // check if the position to drop is a legal diagonal move
-  let isDiagonal = isDiagonalMove();
 
-  // check if position to drop is a legal jump move
-  let isJump = isJumpMove();
+  // check if dropLocation has no Piece and piece doesn't become a king
+  if(!isPiece && !becomesKing){
 
-  if(!isPiece){
-
+    //if the move is a jump and not a diagonal move
     if(isJump && !isDiagonal){
 
+      // for black
       if( selectedPiece == 1){
         // draw selected piece at mouse Position
         pieces[mousePos[1]][mousePos[0]] = selectedPiece;
@@ -216,6 +231,7 @@ function pieceDrop(){
         pieces[mousePos[1] - 1][(mousePos[0] + selectedCol)/2] = 0;
       }
 
+      //for white
       if(selectedPiece == 2){
 
         // draw selected piece at mouse Position
@@ -243,6 +259,13 @@ function pieceDrop(){
   // remove the mousedrag listner
   canvas.removeEventListener('mousemove', pieceDrag, false);
 }
+
+
+/*************************************************************/
+/***                                                       ***/
+/***                     CHECK FUNCTIONS                   ***/
+/***                                                       ***/
+/*************************************************************/
 
 
 function hasPiece(){
@@ -328,6 +351,36 @@ function isJumpMove(){
   }
   return isValidJump;
 }
+
+
+function isKing(){
+  getMousePos(event);
+
+  let isKing = false;
+  //for black
+  if(selectedPiece === 1){
+    // if the mouseY position is > 6 the piece becomes King
+    if(mousePos[1] > 6){
+      isKing = true;
+    }
+  }
+
+  //for white
+  if(selectedPiece === 2){
+    // if the mouseY position is > 6 the piece becomes King
+    if(mousePos[1] < 1){
+      isKing = true;
+    }
+  }
+  return isKing;
+}
+
+
+/*************************************************************/
+/***                                                       ***/
+/***                     RENDER FUNCTION                   ***/
+/***                                                       ***/
+/*************************************************************/
 
 
 function render(){
